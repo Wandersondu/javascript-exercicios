353e2d8e785f246c1f55d6781fde827bde1b9632
Wandersondu <wdutra@outlook.pt>
2022-03-21 00:14:23 +0000

    adicionando novo exe

---
+++ Exercicios Basicos/Exe01/script.js
@@ -0,0 +1,6 @@
+alert('Bem-vindo! A seguir pediremos que informe alguns dados.')
+let nome = prompt('Digite seu nome!')
+let idade = prompt('digite sua idade!')
+
+ confirm(`A sua idade ${idade} está correta?`)
+document.write (`Bem vindo ${nome}, a sua idade é ${idade}`)
